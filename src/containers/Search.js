e63f7b0c86361891cb8d4c8dc1c506b773c28c90
erlan artykbaev <erik.2405@mail.ru>
2021-02-10 12:54:32 +0600

    feature: create functionality to search

--- src/containers/Search.js
+++ src/containers/Search.js
@@ -1,8 +1,18 @@
 import React, {useState} from 'react'
+import {useSelector} from 'react-redux'
 
 const Search = () => {
 
+	const products = useSelector(state => state.products)
+
 	const [input, setInput] = useState('')
+	const [items, setItems] = useState('')
+
+	const handleChangeInput = (e) => {
+		setInput(e.target.value)
+		let searched = products.filter(item => item.name.includes(input))
+		setItems([...searched])
+	}
 
 	return(
 		<div className='search'>
@@ -11,9 +21,20 @@ const Search = () => {
 				className='search-input' 
 				placeholder='Я ищу...'
 				value={input}
-				onChange={e => setInput(e.target.value)}
+				onChange={e => handleChangeInput(e)}
 			/>
 			<button className='btn-search'>Найти</button>
+			{
+				input && items ? (
+					<div className='searched-items' >
+						{
+							items.lenght !== 0 ? items.map(items => (
+								<div>{items.hello}</div>
+							)) : (<div>Не удалось найти</div>)
+						}
+					</div>
+				) : null
+			}
 		</div>
 	)
 }
