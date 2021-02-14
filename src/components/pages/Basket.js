08f33203afa7c0d7a4d7961c6ffaefd38d280f78
erlan artykbaev <erik.2405@mail.ru>
2021-02-14 14:12:24 +0600

    feature: tabs for cart with styling, bugfix with z-indexes from navigation tool

--- src/components/pages/Basket.js
+++ src/components/pages/Basket.js
@@ -1,20 +1,29 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import {Tabs, TabList, Tab, TabPanel} from 'react-tabs'
 
 import ShoppingCart from '../../assets/img/shopping_basket1.svg'
 import Edit from '../../assets/img/edit1.svg'
 import Car from '../../assets/img/car.svg'
+import yellowTabFirst from '../../assets/img/yellow-tab-first.svg'
+import yellowTabSecond from '../../assets/img/yellow-tab-second.svg'
+import greyTabFirst from '../../assets/img/grey-tab-first.svg'
+import greyTabSecond from '../../assets/img/grey-tab-second.svg'
 
 const Basket = () => {
+
+	const [tabIndex, setTabIndex] = useState(0)
 
 	return(
 		<div className='basket-page'>
 			<div className='page-wrapper'>
-				<Tabs>
+				<Tabs
+					selectedIndex={tabIndex}
+					onSelect={index => setTabIndex(index)}
+				>
 					<TabList className='tabs'>
-						<CustomTab img={ShoppingCart} text='Корзина' />
-						<CustomTab img={Edit} text='Оформление заказа' />
-						<CustomTab img={Car} text='Доставка товара' />
+						<CustomTab bg={tabIndex === 0 ? yellowTabFirst : greyTabFirst} img={ShoppingCart} text='Корзина' />
+						<CustomTab bg={tabIndex === 1 ? yellowTabSecond : greyTabSecond} img={Edit} text='Оформление заказа' />
+						<CustomTab bg={tabIndex === 2 ? yellowTabSecond : greyTabSecond} img={Car} text='Доставка товара' />
 					</TabList>
 					<TabPanel>first 1</TabPanel>
 					<TabPanel>second 2</TabPanel>
@@ -30,9 +39,10 @@ const Basket = () => {
 
 const CustomTab = (props) => {
 	return(
-		<Tab className={'tab-item'}>
+		<Tab className={'tab-item'} >
+			<img src={props.bg} alt='backgroundImage' className='bg-img' />
 			<img src={props.img} alt='basket' className='basket-page-icon-tab' />
-			<div>{props.text}</div>
+			<div className='text'>{props.text}</div>
 		</Tab>
 	)
 }
