594e066bc0e3e5df040aad4077397c8109649a35
eartykbaev <erik.2405@mail.ru>
2021-03-27 12:14:26 +0600

    refactor: sum in som not ruble

--- src/containers/Basket.js
+++ src/containers/Basket.js
@@ -3,13 +3,11 @@ import {useSelector} from 'react-redux'
 import {NavLink} from 'react-router-dom'
 
 import icBasket from '../assets/img/ic_bascket.svg'
-import useWindowDimensions from '../helpers/useWindowDimension'
- 
+
 const Basket = () => {
 	const [show, setShow] = useState(false)
 	const basket = useSelector(state => state.basket)
 	const totalSum = useSelector(state => state.basket.totalPriceToBuy)
-	const {width} = useWindowDimensions()
 
 	return(
 		<div className='basket' onClick={() => setShow(!show)}>
@@ -19,7 +17,7 @@ const Basket = () => {
 					<div className='ic-basket-count'>{basket.items.length}</div> 
 				) : null
 			}
-			<div className='total-sum'>{totalSum.toFixed(2)} руб</div>
+			<div className='total-sum'>{totalSum.toFixed(2)} сом</div>
 			{
 				show ? (
 					<div className='basket-dropdown'>
