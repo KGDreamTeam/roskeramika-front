f7ea77002a6c4eaf25b3a41b742d5f1c50829693
eartykbaev <erik.2405@mail.ru>
2021-03-27 13:51:14 +0600

    category page is ready

--- src/components/pages/Category.js
+++ src/components/pages/Category.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from '../../axios/axios'
 
-const Category = () => {
+import Collection from "../templates/Collection";
+
+const Category = (props) => {
+	const [collections, setCollections] = useState([])
+
+	useEffect(() => {
+		const i = props.match.params.id
+		axios.get(`/apiv1/collection/?categories=${i}`)
+			.then(res => {
+			  const data = res.data
+				setCollections([...data])
+			})
+			.catch(err => {
+				console.log(err)
+			})
+	}, [props.match.params.id])
+
 	return(
 		<div className='category-page'>
-			category
+      <div className='category-page-container'>
+				{
+					collections && collections.map(item => (
+						<Collection
+							img={item.image1}
+							name={item.name}
+							key={item.id}
+							id={item.id}
+							sales={item.discount}
+							products={item.products}
+						/>
+					))
+				}
+			</div>
 		</div>
 	)
 }
