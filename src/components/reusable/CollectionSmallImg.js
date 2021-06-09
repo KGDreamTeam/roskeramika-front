a4a4988e09c2423c0d67b591e580d4f6cc5652fc
eartykbaev <erik.2405@mail.ru>
2021-06-10 02:30:57 +0600

    feature: choose photo and zoom posibility

--- src/components/reusable/CollectionSmallImg.js
+++ src/components/reusable/CollectionSmallImg.js
@@ -1,15 +1,20 @@
-import React from "react"
+import React from "react";
 
 const CollectionSmallImg = (props) => {
   const handleClick = () => {
-    // console.log("clickced")
-    props.chooseImg(props.index)
-  }
+    props.chooseImg(props.src);
+  };
   return (
-    <div onClick={handleClick} className='small-image-wrapper selected'>
-      <img src={props.src} alt={props.src} />
+    <div
+      onClick={handleClick}
+      className={
+        props.src === props.choosenImg
+          ? "small-image-wrapper selected"
+          : "small-image-wrapper"
+      }>
+      <img className="img" src={props.src} alt={props.src} />
     </div>
-  )
-}
+  );
+};
 
-export default CollectionSmallImg
+export default CollectionSmallImg;
--- src/components/templates/CollectionInfo.js
+++ src/components/templates/CollectionInfo.js
@@ -1,35 +1,42 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react";
 // import Zoom from "react-img-zoom"
 // import InnerImageZoom from "react-inner-image-zoom"
+import { TransformComponent, TransformWrapper } from "react-zoom-pan-pinch";
 
-import GreenCircle from "../../assets/img/iconfinder_circle.svg"
-import CollectionSmallImg from "../reusable/CollectionSmallImg"
+import GreenCircle from "../../assets/img/iconfinder_circle.svg";
+import CollectionSmallImg from "../reusable/CollectionSmallImg";
 
 const CollectionInfo = (props) => {
-  const [images, setImages] = useState("")
-  const [image, setImage] = useState("")
+  const [images, setImages] = useState("");
+  const [image, setImage] = useState("");
 
   useEffect(() => {
-    setImage(props.img[0])
-    const imagesFiltered = props?.img.filter((item) => item)
-    setImages([...imagesFiltered])
-  }, [props.img])
+    setImage(props.img[0]);
+    const imagesFiltered = props?.img.filter((item) => item);
+    setImages([...imagesFiltered]);
+  }, [props.img]);
 
-  const changeImageHandle = (e) => {
-    console.log(e)
-  }
+  const changeImageHandle = (url) => {
+    setImage(url);
+  };
 
   return (
-    <div className='collection-info'>
-      <h2 className='collection-name'>{props.name}</h2>
-      <div className='content'>
-        <div className='images'>
-          <div className='photo-wrapper'>
-            {/* <Zoom img={imageUrl} zoomScale={3} width={600} height={400} /> */}
-            <img src={image} className='photo' alt={`${props.name}`} />
-            {/* <InnerImageZoom src={props.img} /> */}
+    <div className="collection-info">
+      <h2 className="collection-name">{props.name}</h2>
+      <div className="content">
+        <div className="images">
+          <div className="photo-wrapper">
+            <TransformWrapper
+              defaultScale={1}
+              defaultPositionX={100}
+              defaultPositionY={200}>
+              <TransformComponent>
+                <img src={image} className="photo" alt={`${props.name}`} />
+              </TransformComponent>
+            </TransformWrapper>
+            <div className="img-text">Прокрутка для увеличение</div>
           </div>
-          <div className='small-images'>
+          <div className="small-images">
             {images &&
               images.map((item, index) => (
                 <CollectionSmallImg
@@ -42,43 +49,43 @@ const CollectionInfo = (props) => {
               ))}
           </div>
         </div>
-        <div className='all-info-wrapper'>
-          <div className='price-wrapper'>
-            <span className='price-text'>Цена: </span>
-            <span className='price'>{props.price} сомов</span>
+        <div className="all-info-wrapper">
+          <div className="price-wrapper">
+            <span className="price-text">Цена: </span>
+            <span className="price">{props.price} сомов</span>
           </div>
-          <div className='monufacturer-wrapper'>
-            <span className='manufacturer-text'>Призводитель: </span>
-            <span className='manufacturer'>{props.manufacturer}</span>
+          <div className="monufacturer-wrapper">
+            <span className="manufacturer-text">Призводитель: </span>
+            <span className="manufacturer">{props.manufacturer}</span>
           </div>
-          <div className='size-wrapper'>
-            <span className='size-text'>Размер: </span>
-            <span className='size'>{props.size}</span>
+          <div className="size-wrapper">
+            <span className="size-text">Размер: </span>
+            <span className="size">{props.size}</span>
           </div>
-          <div className='surface-wrapper'>
-            <span className='surface-text'>Поверхность: </span>
-            <span className='surface'>{props.surface}</span>
+          <div className="surface-wrapper">
+            <span className="surface-text">Поверхность: </span>
+            <span className="surface">{props.surface}</span>
           </div>
-          <div className='style-wrapper'>
-            <span className='style-text'>Стиль: </span>
-            <span className='style'>{props.material}</span>
+          <div className="style-wrapper">
+            <span className="style-text">Стиль: </span>
+            <span className="style">{props.material}</span>
           </div>
-          <div className='dots'></div>
-          <div className='post-wrapper'>
-            <div className='post'>Поделиться:</div>
-            <div className='collection-is-true'>
+          <div className="dots"></div>
+          <div className="post-wrapper">
+            <div className="post">Поделиться:</div>
+            <div className="collection-is-true">
               <img
                 src={GreenCircle}
-                alt='green-circle'
-                className='green-circle'
+                alt="green-circle"
+                className="green-circle"
               />
-              <span className='dostupno'>Коллекция доступна для заказа</span>
+              <span className="dostupno">Коллекция доступна для заказа</span>
             </div>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CollectionInfo
+export default CollectionInfo;
